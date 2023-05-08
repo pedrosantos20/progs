c666196ae50c177450e747cafcb4e0edd221b213
pedrosantos20 <ph9msantos@gmail.com>
2023-05-08 20:21:20 -0300

    changes

--- faculdade/web_JS/exercicio1/script.js
+++ faculdade/web_JS/exercicio1/script.js
@@ -1,8 +1,8 @@
 let fn = prompt("Digite seu Primeiro Nome: ");
 let fl = prompt("Digite seu Último Nome: ");
 let i = prompt("Digite sua idade: ");
-alert(fn + " " + fl + ", sua idade é " + i + " anos.");
+alert(`${fn} ${fl} sua idade é ${i} anos`);
 let d = i * 365;
-console.log(fn + " " + fl + ", sua idade é " + i + " anos, o que equivale aproximadamente " + d + " dias");
+console.log(`${fn} ${fl} sua idade é ${i} anos, o que equivale aproximadamente ${d} dias`);
 
 
